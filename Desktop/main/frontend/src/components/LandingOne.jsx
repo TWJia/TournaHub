fa51f452706d2d6f7a63d606699db36f484a0c67
Tan Wen Jia <tanwenjia998@hotmail.com>
2023-12-17 02:55:55 +0800

    added fake news in home

---
+++ Desktop/main/frontend/src/components/LandingOne.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css";
+
+export default function LandingOne() {
+  return (
+    <div className="landingone-container">
+      <div className="landingone-div"></div>
+    </div>
+  );
+}

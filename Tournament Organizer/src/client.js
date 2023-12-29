53cd4f9cb0f84f98d48908b62cdcef3be1f63424
weixuanasc <weixuanasc@gmail.com>
2023-12-29 22:06:32 +0800

    Demo Prototype
    Latest version of the prototype as shown in the demo on 23/12/2023

--- Tournament Organizer/src/client.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const client = axios.create({
-    baseURL:" http://localhost:3001/",
-    // baseURL:"https://dcbasborneoabadiselalu.glitch.me/",
-})
-
-export default client

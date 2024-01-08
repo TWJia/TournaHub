d6430fb7aa30f531e83a8dfcfb4bd17001086817
TWJia <131585804+TWJia@users.noreply.github.com>
2024-01-09 01:40:51 +0800

    Add files via upload
    Fix category bug

--- frontend/src/components/Applicant/NewsForm.jsx
+++ frontend/src/components/Applicant/NewsForm.jsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import NavbarA from "./NavbarA";
 
 const NewsForm = () => {
+  // const [image, setImage] = useState("");
+  const [sports, setSports] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const [newsData, setnewsData] = useState({
     category: "",
     title: "",
     content: "",
-
+    author: "",
     image: "",
   });
 
@@ -18,12 +24,46 @@ const NewsForm = () => {
       };
     });
   };
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:3001/getCurrentUser");
+      setUser(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    const fetchSports = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/ManageSports");
+        setSports(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSports();
+    fetchData();
+  }, []);
+
   const handleSubmit = async () => {
     try {
+      const category = document.querySelector('select[name="category"]').value;
+      setnewsData({
+        ...newsData,
+        category: category,
+      });
       const { status } = await axios.post(
         "http://localhost:3001/api/news/create",
         newsData
       );
+      if (status === 200) {
+        alert("News created successfully!");
+      } else {
+        alert("Failed to create news. Please try again.");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -31,27 +71,37 @@ const NewsForm = () => {
 
   return (
     <div>
-      <h1>new form </h1>
+      <NavbarA />
+      <h1>Write a new article:</h1>
       <form action="">
-        <select name="category" id="" onChange={handleInputChange}>
-          <option value="category">category</option>
-          <option value="Soccer">Soccer</option>
-          <option value="Badminton">Badminton</option>
-          <option value="Basketball">Basketball</option>
+        <h6>Select sport category:</h6>
+        <select type="select" name="category" onChange={handleInputChange}>
+          {sports.map((sport) => (
+            <option key={sport.id} value={sport.name}>
+              {sport.name}
+            </option>
+          ))}
         </select>
       </form>
       <input
         type="text"
         name="title"
         id=""
-        placeholder="enter a title"
+        placeholder="Enter a title"
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="author"
+        id=""
+        placeholder="Enter Author name"
         onChange={handleInputChange}
       />
       <input
         type="text"
         name="content"
         id=""
-        placeholder="enter the content"
+        placeholder="Enter the article content"
         onChange={handleInputChange}
       />
       <input

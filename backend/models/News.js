ea1074eef13c2f87cb981125e8934a67dac2ba18
TWJia <131585804+TWJia@users.noreply.github.com>
2024-01-09 01:42:30 +0800

    Add files via upload
    Fix category bug

--- backend/models/News.js
+++ backend/models/News.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 const NewsSchema = new mongoose.Schema({
   title: String,
+  author: String,
   content: String,
   image: String,
   category: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  postDate: { type: Date, default: Date.now },
 });
 
 const NewsModel = mongoose.model("news", NewsSchema);
